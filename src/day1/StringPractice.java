863f6869b0e1a0e9b1461280b98a1eb0c4221e14
sezginhmrc <sezginhamurcu@icloud.com>
2020-02-29 13:43:40 -0500

    my first commit

---
+++ src/day1/StringPractice.java
@@ -0,0 +1,23 @@
+package day1;
+
+public class StringPractice {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, world!");
+
+
+        // How to send this into my GitHub ?
+
+        // Git local repository and remote repository
+
+        // Local is on my computer
+        // We create local repository for specific project
+        // Then We create remote repository on Github. Last Step is connectin them
+        // Any git project starts with one command :
+        // GIT INIT -> this command will create local repository for you java Project
+        // Folder -> GitPracticeFall2019
+        // init - it is  a short for initialize
+
+    }
+}
