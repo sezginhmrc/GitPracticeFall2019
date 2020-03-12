632f323cc108d3e7670c98234afd661f1a703b67
sezginhmrc <sezginhamurcu@icloud.com>
2020-03-11 20:14:55 -0400

    Added Colors Class

---
+++ src/day1/Colors.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class Colors {
+
+    public static void main(String[] args) {
+
+        String [] colors = {"Manchester is RED ! ", "NOT BLUE", "GIRESUN IS GREEN"};
+
+    }
+}
