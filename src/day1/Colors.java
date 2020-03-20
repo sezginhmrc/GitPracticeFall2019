cce5f401d2e45472402feb61525742925b99e446
sezginhmrc <sezginhamurcu@icloud.com>
2020-03-20 13:01:18 -0400

    added colors

--- src/day1/Colors.java
+++ src/day1/Colors.java
@@ -1,5 +1,7 @@
 package day1;
 
+import java.util.Arrays;
+
 public class Colors {
 
     public static void main(String[] args) {
@@ -7,5 +9,7 @@ public class Colors {
         String [] colors = {"Manchester is RED ! ", "NOT BLUE", "GIRESUN IS GREEN", "BLACK","WHITE","YELLOW"};
 
         System.out.println("MY COLORS");
+
+        System.out.println(Arrays.toString(colors));
     }
 }
