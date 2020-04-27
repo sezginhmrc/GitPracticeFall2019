b2d244494e9176fe29eee85bca6b3b0286818e52
sezginhmrc <sezginhamurcu@icloud.com>
2020-04-27 04:36:57 -0400

    Added

---
+++ src/day1/StashingClass.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class StashingClass {
+
+    public static void main(String[] args) {
+
+        String esma = "IKRO" ;
+
+        System.out.printf("", esma.substring(0, 1));
+    }
+
+
+}
